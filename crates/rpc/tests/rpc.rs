b5829c21ee481ca80360e3d832d172ab470dd7aa
pierre-l <pierre.larger@gmail.com>
2024-04-24 16:28:50 +0200

    block_hash_and_number

--- crates/rpc/tests/rpc.rs
+++ crates/rpc/tests/rpc.rs
@@ -1,5 +1,5 @@
 // These tests need Beerus to run in the background, hence why they're hidden behind the following feature.
-#![cfg(feature = "integration-tests")]
+// TODO #![cfg(feature = "integration-tests")]
 
 use beerus_core::config::DEFAULT_PORT;
 use cached::Cached;
@@ -9,10 +9,7 @@ use cached::SizedCache;
 use reqwest::Url;
 use starknet::{
     core::types::{
-        BlockId, BlockWithTxHashes, BlockWithTxs, DeclareTransaction,
-        DeployAccountTransaction, FieldElement, InvokeTransaction,
-        MaybePendingBlockWithTxHashes, MaybePendingBlockWithTxs,
-        MaybePendingTransactionReceipt, Transaction, TransactionReceipt,
+        BlockHashAndNumber, BlockId, BlockWithTxHashes, BlockWithTxs, DeclareTransaction, DeployAccountTransaction, FieldElement, InvokeTransaction, MaybePendingBlockWithTxHashes, MaybePendingBlockWithTxs, MaybePendingTransactionReceipt, Transaction, TransactionReceipt
     },
     providers::{
         jsonrpc::HttpTransport, JsonRpcClient, Provider, ProviderError,
@@ -160,6 +157,34 @@ async fn context<F: Fn(&BlockWithTxs) -> Option<T>, T>(
 }
 
 // starknet_blockNumber is already tested in the creation of the test context.
+
+#[tokio::test]
+async fn test_block_hash_and_number() {
+    async fn run() -> Result<(), &'static str> {
+        let TestContext { client, block, block_id: _, extracted_value: () } =
+            latest_block_context().await;
+
+        let BlockHashAndNumber { block_hash, block_number } = client
+            .block_hash_and_number()
+            .await
+            .expect("Failed to retrieve the block hash & number");
+
+        if block_number != block.block_number {
+            return Err("Block number mismatch");
+        }
+        if block_hash != block.block_hash {
+            return Err("Block hash mismatch");
+        }
+
+        Ok(())
+    }
+
+    // There is a very slight chance the test occurs right when a new block is produced, making the assertions fail.
+    // Just run the test twice.
+    if run().await.is_err() {
+        run().await.expect("Block hash or number mismatch")
+    }
+}
 
 #[tokio::test]
 async fn test_chain_id() {
